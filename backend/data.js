a4349c647606d97b72455a4115d4e4d3eca67cb9
Jesse McReady <jessemcready@gmail.com>
2019-01-16 12:12:26 -0500

    changed schema to hold users

--- backend/data.js
+++ backend/data.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs') 
 const Schema = mongoose.Schema 
 
-const ExpenseSchema = new Schema(
-    {
+const UserSchema = new Schema({
         id: Number, 
         name: String,
-        price: Number
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String, 
+            required: true
+        },
+        expenses: [
+            { name: { type: String, require: true }, price: { type:Number, required:true }}
+        ]
     },
     { timestamps: true }
 )
 
-module.exports = mongoose.model("Expense", ExpenseSchema)
+const User = module.exports = mongoose.model("User", UserSchema)
+
+module.exports.getUserById = (id, callback) => {
+    User.findById(id, callback) 
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+    const query = { username: username }
+    User.findOne(query, callback) 
+}
+
+module.exports.addUser = (newUser, callback) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err 
+            newUser.password = hash 
+            newUser.save(callback) 
+        })
+    })
+}
+
+module.exports.comparePassword = (password, hash, callback) => {
+    bcrypt.compare(password, hash, (err, isMatch) => {
+        if(err) throw err 
+        callback(null, isMatch)
+    })
+}
