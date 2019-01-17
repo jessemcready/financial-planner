d05628f96c716464643d2aedb0c38d8c775bd8d6
Jesse McReady <jessemcready@gmail.com>
2019-01-16 22:58:36 -0500

    coloring

--- client/src/styled_components/navList.js
+++ client/src/styled_components/navList.js
@@ -7,9 +7,9 @@ const NavList = styled.ul`
     align-items: center;
     width: 100%;
     height: 46px;
-    box-shadow: 5px 10px 20px -20px rgba(85,172,238 ,1);
+    box-shadow: 5px 10px 20px -20px rgba(85, 172, 238, 1);
     padding: 0 10px;
-    border: 1px solid rgba(238,238,238 ,1);
+    border: 1px solid rgba(238, 238, 238, 1);
     border-radius: 5px;
     color: rgb(133,187,101);
 `
